1f7e0fc44e0debfc3e6caa943a3965f8709b7460
bquirke <bquirke@tcd.ie>
2025-08-27 13:42:23 +0100

    Initial commit

---
+++ src/test/java/com/bryansparactising/addressbook/AddressbookApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bryansparactising.addressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Crap");
+	}
+
+}
